6a0764eb5176e09b0a96dc8e3c69a0115dadcdb1
karol <karoljanowski@yahoo.com>
2024-03-02 13:30:11 +0100

    lomboked classes

--- src/main/java/pl/zajavka/mortgage/model/MortgageReference.java
+++ src/main/java/pl/zajavka/mortgage/model/MortgageReference.java
@@ -1,6 +1,11 @@
 package pl.zajavka.mortgage.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.math.BigDecimal;
+@Getter
+@AllArgsConstructor
 
 public class MortgageReference {
 
@@ -8,16 +13,4 @@ public class MortgageReference {
 
     private final BigDecimal referenceDuration;
 
-    public MortgageReference(final BigDecimal referenceAmount, final BigDecimal referenceDuration) {
-        this.referenceAmount = referenceAmount;
-        this.referenceDuration = referenceDuration;
-    }
-
-    public BigDecimal getReferenceAmount() {
-        return referenceAmount;
-    }
-
-    public BigDecimal getReferenceDuration() {
-        return referenceDuration;
-    }
 }
