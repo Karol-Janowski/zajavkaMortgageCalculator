9a71b80b46b5a117c662d96822b0f97b7a67b56f
karol <karoljanowski@yahoo.com>
2024-03-02 13:34:35 +0100

    lomboked classes

--- src/main/java/pl/zajavka/mortgage/model/MortgageReference.java
+++ src/main/java/pl/zajavka/mortgage/model/MortgageReference.java
@@ -1,11 +1,12 @@
 package pl.zajavka.mortgage.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
+
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 
 public class MortgageReference {
 
