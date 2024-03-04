bdb62a47c50a5d0e847484d0985bc195c450bfee
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-04 18:16:04 +0100

    lombok exercise

--- src/main/java/pl/zajavka/mortgage/model/Overpayment.java
+++ src/main/java/pl/zajavka/mortgage/model/Overpayment.java
@@ -1,19 +1,22 @@
 package pl.zajavka.mortgage.model;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
+@Getter
 public class Overpayment {
 
+    @Getter(AccessLevel.NONE)
     public static final String REDUCE_RATE = "REDUCE_RATE";
-
+    @Getter(AccessLevel.NONE)
     public static final String REDUCE_PERIOD = "REDUCE_PERIOD";
 
-    @Getter
+
     private final BigDecimal amount;
 
-    @Getter
+
     private final BigDecimal provisionAmount;
 
     public Overpayment(final BigDecimal amount, final BigDecimal provisionAmount) {
